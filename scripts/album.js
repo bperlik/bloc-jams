5170201291e662a69745641a6db6c772e0f16517
Barbara Perlik <barbperlik@gmail.com>
2016-10-27 23:09:56 -0400

    Add mouse over play button on album page

--- scripts/album.js
+++ scripts/album.js
@@ -35,7 +35,7 @@
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -66,10 +66,29 @@ var setCurrentAlbum = function(album) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
- 
+ var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+ var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
- };
+	  songListContainer.addEventListener('mouseover', function(event) {
+         // stores the DOM element WHERE the event occurred
+       // Only target individual song rows during event delegation
+     if (event.target.parentElement.className === 'album-view-song-item') {
+        event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         } //end if 
+     });	 //end songList
+	 for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+         // Selects first child element, which is the song-item-number element
+         this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+	 
+ }; //end window onload
 
 
 
